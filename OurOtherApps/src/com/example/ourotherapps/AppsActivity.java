b5d7d58520ab03bfc7e326ed61500096fe2af141
Vicent Pérez <vicennt@gmail.com>
2014-03-18 12:15:09 +0100

    Commit Link Apps

--- OurOtherApps/src/com/example/ourotherapps/AppsActivity.java
+++ OurOtherApps/src/com/example/ourotherapps/AppsActivity.java
@@ -1,20 +1,51 @@
 package com.example.ourotherapps;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+public class AppsActivity extends ListActivity {
 
-public class AppsActivity extends Activity{
-	
+	private String[] values = new String[] { "Chat Toolkit", "Spell Checker",
+			"Lottodroid", "Remote Gallery 3D" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_apps);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, values);
+		setListAdapter(adapter);
 	}
-	
 
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		switch (position) {
+		case 0:
+			openApp("com.androidsx.smileys");
+			break;
+		case 1:
+			openApp("com.androidsx.checkspelling");
+			break;
+		case 2:
+			openApp("com.androidsx.lottodroid");
+			break;
+		case 3:
+			openApp("com.androidsx.easygallery");
+			break;
+
+		}
+	}
+
+	private void openApp(String app) {
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setData(Uri.parse("market://details?id="+app));
+		startActivity(intent);
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,6 +53,5 @@ public class AppsActivity extends Activity{
 		getMenuInflater().inflate(R.menu.apps, menu);
 		return true;
 	}
-
 
 }
