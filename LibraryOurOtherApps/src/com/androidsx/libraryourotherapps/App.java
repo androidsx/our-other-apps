fab1191829711abf2ab5e371f98fa15bd6a9f91f
Vicent Pérez <vicennt@gmail.com>
2014-03-25 00:26:32 +0100

    Restore files

--- LibraryOurOtherApps/src/com/androidsx/libraryourotherapps/App.java
+++ LibraryOurOtherApps/src/com/androidsx/libraryourotherapps/App.java
@@ -1,9 +1,5 @@
 package com.androidsx.libraryourotherapps;
 
-<<<<<<< HEAD
-import java.io.Serializable;
-=======
-<<<<<<< HEAD
 import java.io.Serializable;
 
 
@@ -26,36 +22,6 @@ public class App implements Serializable {
 
 	public int getIconResId() {
 		return iconResId;
-=======
-import android.graphics.drawable.Drawable;
->>>>>>> master
-
-
-public class App implements Serializable {
-
-
-	private static final long serialVersionUID = -6395743145003950777L;
-	private int iconResId;
-	private String title;
-	private String description;
-	private String packageName;
-
-	public App(int iconResId, String title, String description, String packageName) {
-		this.iconResId = iconResId;
-		this.title = title;
-		this.description = description;
-		this.packageName = packageName;
-	}
-
-
-<<<<<<< HEAD
-	public int getIconResId() {
-		return iconResId;
-=======
-	public Drawable getPicture() {
-		return picture;
->>>>>>> 87ae55bbb0c0a85f26b376906d7599f1ab4df582
->>>>>>> master
 	}
 
 	public String getTitle() {
@@ -66,19 +32,10 @@ public class App implements Serializable {
 	public String getDescription() {
 		return description;
 	}
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> master
 	
 	public String getPackageName() {
 		return packageName;
 	}
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 87ae55bbb0c0a85f26b376906d7599f1ab4df582
->>>>>>> master
 
 
 }
