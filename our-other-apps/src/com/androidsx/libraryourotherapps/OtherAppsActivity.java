bf83648257a827a1e42d5bf7529fea9c98e21a06
Vicent Pérez <vicennt@gmail.com>
2014-06-06 14:35:42 +0200

    Get array list app from xml file

--- our-other-apps/src/com/androidsx/libraryourotherapps/OtherAppsActivity.java
+++ our-other-apps/src/com/androidsx/libraryourotherapps/OtherAppsActivity.java
@@ -1,6 +1,7 @@
 package com.androidsx.libraryourotherapps;
 
 import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -9,53 +10,61 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class OtherAppsActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_apps);
-		Intent intent = getIntent();
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps);
+        Intent intent = getIntent();
 
-		ListView listview = (ListView) findViewById(R.id.list);
-		ArrayList<App> apps = (ArrayList<App>) intent
-				.getSerializableExtra("listapp");
+        ListView listview = (ListView) findViewById(R.id.list);
+        final TextView txtTitle = (TextView) findViewById(R.id.check);
 
-		configureAppList(listview, apps);
-	}
+        int textColorTitle = intent.getIntExtra("textColorTitle", 0);
+        String title = intent.getStringExtra("title");
+        txtTitle.setText(title);
+        txtTitle.setTextColor(textColorTitle);
 
-	private void configureAppList(ListView listview,
-			final ArrayList<App> listApps) {
+        ArrayList<App> apps = (ArrayList<App>) intent.getSerializableExtra("listapp");
 
-		AppAdapter adapter = new AppAdapter(this, listApps);
+        // View view = this.getWindow().getDecorView();
+        // int backgroundColor = intent.getIntExtra("backgroundColor", 0);
+        // view.setBackgroundColor(backgroundColor);
 
-		listview.setAdapter(adapter);
-		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> adapter, View view,
-					int position, long id) {
-				openApp(listApps.get(position).getPackageName());
-			}
-		});
-	}
+        configureAppList(listview, apps);
+    }
 
-	private void openApp(String packageName) {
-		try {
-			startActivity(new Intent(Intent.ACTION_VIEW,
-					Uri.parse("market://details?id=" + packageName)));
-		} catch (android.content.ActivityNotFoundException anfe) {
-			startActivity(new Intent(Intent.ACTION_VIEW,
-					Uri.parse("http://play.google.com/store/apps/details?id="
-							+ packageName)));
-		}
-	}
+    private void configureAppList(ListView listview, final ArrayList<App> listApps) {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        AppAdapter adapter = new AppAdapter(this, listApps);
+
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+                openApp(listApps.get(position).getPackageName());
+            }
+        });
+    }
+
+    private void openApp(String packageName) {
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id="
+                    + packageName)));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
